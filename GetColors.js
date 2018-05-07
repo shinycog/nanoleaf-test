985f173b883b7386e5e757a3623f70982a88d19c
shinycog <38968512+shinycog@users.noreply.github.com>
2018-05-06 23:51:46 -0500

    GetColors returns hue values
    Hue values are 0-360, when panels are set to their respective named colors, only blue returns a nonzero value, 242.

--- GetColors.js
+++ GetColors.js
@@ -1,8 +1,12 @@
 const controllers = require("./AuroraControllers");
+    controllers.Blue.hue().then(info => {console.log("Blue : " + info)});
+    controllers.Purple.hue().then(info => {console.log("Purple : " + info)});
+    controllers.Yellow.hue().then(info => {console.log("Yellow : " + info)});
+    controllers.Green.hue().then(info => {console.log("Green : " + info)});
 
-for(var controller in controllers){
-    controllers[controller].hue().then(info => {
-        console.log(controller + ": " + info);
-    });
 
-}
+// for(var controller in controllers){
+//     console.log(controller);
+//     controllers[controller].hue().then(info => {
+//         console.log(controller + ": " + info);
+//     });}

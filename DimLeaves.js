33ca99bab83eb957ae7eac2bb2a21e1c8edde3c9
Anthony Lee <anthony.mi.lee@outlook.com>
2018-05-06 22:56:56 -0500

    Added a file to dim the leaves

---
+++ DimLeaves.js
@@ -0,0 +1,8 @@
+const controllers = require("./AuroraControllers");
+// controllers.Blue.off();
+
+console.log(controllers);
+for(var controller in controllers){
+   console.log(controller);
+    controllers[controller].setBrightness(10);
+}

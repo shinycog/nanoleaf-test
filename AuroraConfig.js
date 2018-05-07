f593ccca22d76d77a2dd632d49e7098da879d737
Anthony Lee <anthony.mi.lee@outlook.com>
2018-05-06 22:30:05 -0500

    Added configurations for 4 auroras

--- AuroraConfig.js
+++ AuroraConfig.js
@@ -1,4 +1,25 @@
-let AuroraConfig = {};
+let AuroraConfig = {
+    "blue":{
+        "ip":"192.168.1.85",
+        "mac":"00:55:DA:52:1E:50",
+        "token":"EXuFadXklBtNcC4NjqWKTw55ldXBpVc2"
+    },
+    "purple":{
+        "ip":"192.168.1.86",
+        "mac":"00:55:DA:51:4D:88",
+        "token":"YLYPwVVQ5cPpKkXuu1DcdVT5owe8OIEa"
+    },
+    "yellow":{
+        "ip":"192.168.1.82",
+        "mac":"00:55:DA:52:1E:44",
+        "token":"y9jxpcgUrDLqN4BAHPX0Ckl82x6JxJ7u"
+    },
+    "green":{
+        "ip":"192.168.1.87",
+        "mac":"00:55:DA:51:E3:02",
+        "token":"rY7yEVZcUc1not0x3W228gbnlecfCOx3"
+    }
+};
 module.exports = {
     AuroraConfig
 }

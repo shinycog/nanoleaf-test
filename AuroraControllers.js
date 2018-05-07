32632597b9214f1c5ca74b6e3ca288da29bcb076
shinycog <38968512+shinycog@users.noreply.github.com>
2018-05-06 22:41:01 -0500

    Fleshed out AuroraControllers

--- AuroraControllers.js
+++ AuroraControllers.js
@@ -1,12 +1,30 @@
 let auroraConfig = require("./AuroraConfig").AuroraConfig
 let AuroraAPI = require("nanoleaves")
 
-// Create Blue
-const blue = new AuroraAPI({
+//Create controllers by color name, based on AuroraConfig
+const Blue = new AuroraAPI({
     host: auroraConfig.blue.ip,
     token: auroraConfig.blue.token
 });
 
+const Purple = new AuroraAPI({
+    host: auroraConfig.purple.ip,
+    token: auroraConfig.purple.token
+});
+
+const Yellow = new AuroraAPI({
+    host: auroraConfig.yellow.ip,
+    token: auroraConfig.yellow.token
+});
+
+const Green = new AuroraAPI({
+    host: auroraConfig.green.ip,
+    token: auroraConfig.green.token
+});
+
 module.exports = {
-    Blue: blue
+    Blue,
+    Purple,
+    Yellow,
+    Green
 }
